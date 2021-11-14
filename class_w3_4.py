49b73096093ef304cdd3168c9e0d41be2324d3b9
zerpaivan <zerpaivan@gmail.com>
2021-11-14 19:12:40 -0400

    exercise 5 ready

--- class_w3_4.py
+++ class_w3_4.py
@@ -1,4 +1,5 @@
-# Write a Python class to get all possible unique subsets from a set of distinct
+# Write a Python class to get all possible unique subsets from a set
+# of distinct
 # integers. Go to the editor
 # Input : [4, 5, 6]
 # Output : [[], [6], [5], [5, 6], [4], [4, 6], [4, 5], [4, 5, 6]]
---
+++ class_w3_5.py
@@ -0,0 +1,35 @@
+# Write a Python class to find a pair of elements (indices of the two numbers)
+# from a given array whose sum equals a specific target number.
+# Note: There will be one solution for each input and do not use the
+# same element twice.
+# Input: numbers= [10,20,10,40,50,60,70], target=50
+# Output: 3, 4
+
+
+# the solution from the previous exercise is imported.
+# This returns all the combinations that can be made with the list
+
+from class_w3_4 import Script_4
+
+
+class Script_5():
+
+    def match_sum(self, num, input_list):
+        comb = Script_4().subsets(input_list)
+
+        result = []
+
+        # all combinations are tested looking for a target match
+        for i in comb:
+
+            if sum(i) == num:
+
+                result = i
+                break
+        return result
+
+
+if __name__ == "__main__":
+
+    listin = [10, 20, 30, 40, 70, 5]
+    print(Script_5().match_sum(55, listin))
