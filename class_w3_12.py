40eb9741723f570c606abf379a5a5697a98e7f68
zerpaivan <zerpaivan@gmail.com>
2021-11-24 00:09:49 -0400

    exercise 12 ready

---
+++ class_w3_12.py
@@ -0,0 +1,19 @@
+# Write a Python class named Circle constructed by a radius and two methods
+# which will compute the area and the perimeter of a circle
+
+class Name_1():
+    pass
+
+
+class Name_2():
+    pass
+
+
+def class_name(obj):
+    print(obj.__class__.__name__)
+
+
+prueba_1 = Name_1()
+prueba_2 = Name_2()
+
+class_name(prueba_2)
